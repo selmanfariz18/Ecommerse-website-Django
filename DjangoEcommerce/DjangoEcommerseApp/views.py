2b688dc443cd0ef810939208aead879e1f123b3e
selmanfariz18 <salmanulfarizyka@gmail.com>
2023-04-07 15:03:49 +0530

    setting admin html

--- DjangoEcommerce/DjangoEcommerseApp/views.py
+++ DjangoEcommerce/DjangoEcommerseApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def demopage(request):
+    return HttpResponse("demo page")
+
+def demopageTemplate(request):
+    return render(request,"demo.html")
+
+def adminLogin(request):
+    return render(request,"admin_templates/signin.html")
