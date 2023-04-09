1414e0a2b31add56823c3f08cb2665bf1627e0cb
selmanfariz18 <salmanulfarizyka@gmail.com>
2023-04-10 05:08:23 +0530

    category section added

--- DjangoEcommerce/DjangoEcommerce/urls.py
+++ DjangoEcommerce/DjangoEcommerce/urls.py
@@ -26,5 +26,8 @@ urlpatterns = [
     path('admin_login_process',views.adminLoginProcess,name="admin_login_process"),
     path('admin_logout_process',views.adminLogoutProcess,name="admin_logout_process"),
     #admin page
-    path('admin_home',AdminViews.admin_home,name="admin_home")
+    path('admin_home',AdminViews.admin_home,name="admin_home"),
+    path('category_list',AdminViews.CategoriesListView.as_view(),name="category_list"),
+    path('category_create',AdminViews.CategoriesCreate.as_view(),name="category_create"),
+    path('category_update/<slug:pk>',AdminViews.CategoriesUpdate.as_view(),name="category_update")
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
--- DjangoEcommerce/DjangoEcommerseApp/AdminViews.py
+++ DjangoEcommerce/DjangoEcommerseApp/AdminViews.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView,CreateView,UpdateView,DetailView
+from DjangoEcommerseApp.models import Categories
+from django.contrib.messages.views import SuccessMessageMixin
 
 @login_required(login_url="admin/")
 def admin_home(request):
     return render(request,"admin_templates/home.html")
+
+class CategoriesListView(ListView):
+    model=Categories
+    template_name="admin_templates/category_list.html"
+
+class CategoriesCreate(SuccessMessageMixin,CreateView):
+    model=Categories
+    success_message="Category_added!"
+    fields="__all__"
+    template_name="admin_templates/category_create.html"
+
+class CategoriesUpdate(SuccessMessageMixin,UpdateView):
+    model=Categories
+    success_message="Category_Updated!"
+    fields="__all__"
+    template_name="admin_templates/category_update.html"
